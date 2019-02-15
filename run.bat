8ccc0ac425fa1f2244564cd7c04fee65b4aa90c2
katien1234 <katie.n1234@gmail.com>
2019-02-15 09:45:21 +0000

    Edited names

---
+++ run.bat
@@ -0,0 +1,14 @@
+cd RealAccountApi
+start mvn spring-boot:run
+
+cd ../RealAccountConsumer
+start mvn spring-boot:run
+
+cd ../RealAccountPrizeApi
+start mvn spring-boot:run
+
+cd ../RealAccountNumGenApi
+start mvn spring-boot:run
+
+cd ../AccountFront
+npm start
